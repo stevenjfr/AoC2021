afa0219d533c93a4af99625ff4f67de34a52c70d
stevenjfr <studio@jamman.co.uk>
2021-12-05 12:59:23 +0000

    day 3

--- day3/main.go
+++ day3/main.go
@@ -120,7 +120,7 @@ func pruneRemaining(input [][]int, keep int, position int) [][]int {
 		if v[position] == keep {
 			remaining = append(remaining, v)
 		} else {
-			fmt.Println(v[position], "didnt have ", keep, " as pisition ", position)
+			//	fmt.Println(v[position], "didnt have ", keep, " as pisition ", position)
 		}
 	}
 	//	fmt.Println(remaining)
@@ -148,18 +148,42 @@ func calculateKeep(input [][]int, position int) int {
 	if oneCount > zeroCount {
 		return 1
 
+	} else if oneCount == zeroCount {
+		return 1
+
+	} else {
+		return 0
+	}
+}
+
+func calculateKeepLeast(input [][]int, position int) int {
+	var bits []int
+	for _, line := range input {
+		bits = append(bits, line[position])
+	}
+	var oneCount int
+	var zeroCount int
+
+	for _, bit := range bits {
+
+		if bit > 0 {
+			zeroCount++
+		} else {
+			oneCount++
+		}
+	}
+
+	if oneCount > zeroCount {
+		return 1
+
 	} else {
 		return 0
 
 	}
 }
 
-func code2() {
-
-	lines, _ := readLines("input.txt")
-
+func makeArrayFromText(lines []string) [][]int {
 	var remaining [][]int
-
 	// create a array representation of the text input
 	for _, line := range lines {
 		binary := strings.Split(line, "")
@@ -172,19 +196,52 @@ func code2() {
 		remaining = append(remaining, newArray)
 
 	}
-	//fmt.Println(remaining)
+	return remaining
+
+}
+func code2() {
+
+	lines, _ := readLines("input.txt")
+
+	var remaining = makeArrayFromText(lines)
 
 	//for every bit position
 	for i := 0; i < len(remaining[0]); i++ {
-		//for len(remaining) > 2 {
-		toKeep := calculateKeep(remaining, i)
-		fmt.Printf("keep any values with %s at position %s", toKeep, i)
-		//fmt.Println(toKeep)
-		remaining = pruneRemaining(remaining, toKeep, i)
-		fmt.Println(remaining)
-		//}
+		if len(remaining) > 1 {
+			toKeep := calculateKeep(remaining, i)
+			//	fmt.Printf("keep any values with %s at position %s", toKeep, i)
+			//fmt.Println(toKeep)
+			remaining = pruneRemaining(remaining, toKeep, i)
+			//fmt.Println(remaining)
+		} else {
+			fmt.Println("loop ended")
+			break
+		}
 
 	}
+	var gamma = strings.Trim(strings.Join(strings.Split(fmt.Sprint(remaining), " "), ""), "[]")
+	g, _ := strconv.ParseInt(gamma, 2, 64)
+	fmt.Println(g)
+
+	var remainingLeast = makeArrayFromText(lines)
+
+	//for every bit position
+	for i := 0; i < len(remainingLeast[0]); i++ {
+		if len(remainingLeast) > 1 {
+			toKeep := calculateKeepLeast(remainingLeast, i)
+			//fmt.Printf("keep any values with %s at position %s", toKeep, i)
+			//fmt.Println(toKeep)
+			remainingLeast = pruneRemaining(remainingLeast, toKeep, i)
+			//fmt.Println(remainingLeast)
+		} else {
+			fmt.Println("loop ended")
+			break
+		}
+
+	}
+	var epsilon = strings.Trim(strings.Join(strings.Split(fmt.Sprint(remainingLeast), " "), ""), "[]")
+	e, _ := strconv.ParseInt(epsilon, 2, 64)
+	fmt.Println(e)
 
 }
 func main() {
